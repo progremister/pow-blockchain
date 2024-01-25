73629e75c454f039f14fccc882eacb2d6034fd58
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-01-25 22:56:48 +0100

    Created the mining simulation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use blockchainlib::*; 
 
 fn main() {
-    let mut block = Block::new(0, now(), vec![0; 32], 0, "First Block!".to_owned());
+    let mut block = Block::new(0, now(), vec![0; 32], 118318, "First Block!".to_owned(), 0x0000000ffffffffffffffffffffffffff);
 
-    let hash = block.hash();
-    block.hash = hash;
+    block.hash = block.hash();
+
+    println!("{:?}", &block);
+
+    block.mine();
 
     println!("{:?}", &block);
 }
