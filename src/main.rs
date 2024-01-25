685a11d5ff15c5a4aee17b7a00d64c714ab1f862
Dmytro Kolosovskyi <dmytro.kolosovskyi@student.tuke.sk>
2024-01-25 15:38:45 +0100

    Implemented block hashing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use blockchainlib::*; 
 
 fn main() {
-    let block = Block::new(0, now(), vec![0; 32], 0, "First Block!".to_owned());
+    let mut block = Block::new(0, now(), vec![0; 32], 0, "First Block!".to_owned());
+
+    let hash = block.hash();
+    block.hash = hash;
+
     println!("{:?}", &block);
 }
